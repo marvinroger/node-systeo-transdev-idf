cd0a752a24ca11ed2b22784028682985f9baa0c3
marvinroger <nivramdu94@gmail.com>
2015-02-19 12:06:48 +0000

    Better error handling

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var lines = require('./lib/lines');
 var schedules = require('./lib/schedules');
+var ServerError = require('./lib/errors').ServerError;
 
 module.exports = {
   getLines: lines.getLines,
   getStops: lines.getStops,
-  getSchedules: schedules.getSchedules
+  getSchedules: schedules.getSchedules,
+  ServerError: ServerError
 };
--- test/test.js
+++ test/test.js
@@ -7,8 +7,11 @@ describe('Lines', function(){
   describe('#getLines()', function(){
     it('should return a list of lines (test with J1)', function(done){
       systeoTransdevIDF.getLines(function(err, lines) {
-        should.not.exist(err);
-        lines.should.have.property('J1');
+        if (err instanceof systeoTransdevIDF.ServerError) {
+          console.log(err.message);
+        } else {
+          lines.should.have.property('J1');
+        }
         done();
       });
     });
@@ -17,8 +20,11 @@ describe('Lines', function(){
   describe('#getStops()', function(){
     it('should return a list of stops (test with GARIBALDI)', function(done){
       systeoTransdevIDF.getStops('268435472', function(err, stops) {
-        should.not.exist(err);
-        stops.should.have.property('GARIBALDI');
+        if (err instanceof systeoTransdevIDF.ServerError) {
+          console.log(err.message);
+        } else {
+          stops.should.have.property('GARIBALDI');
+        }
         done();
       });
     });
@@ -31,9 +37,12 @@ describe('Schedules', function(){
   describe('#getSchedules()', function(){
     it('should return a list of schedules', function(done){
       systeoTransdevIDF.getSchedules('268435472', 'GARIBALDI', function(err, schedules) {
-        should.not.exist(err);
-        if (!schedules.noMoreToday) {
-          schedules.should.not.be.empty;
+        if (err instanceof systeoTransdevIDF.ServerError) {
+          console.log(err.message);
+        } else {
+          if (!schedules.noMoreToday) {
+            schedules.should.not.be.empty;
+          }
         }
         done();
       });
